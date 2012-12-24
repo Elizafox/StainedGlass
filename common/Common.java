c6712364610af2b35d0347baa25edc2edb16a387
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2012-12-24 16:01:21 -0600

    Bump to 0.2dev

--- common/Common.java
+++ common/Common.java
@@ -17,7 +17,7 @@ import elizacat.mc.StainedGlass.proxy.BlockStainedPane;
 
 public class Common {
 	public static final String NAME = "StainedGlass";
-	public static final String VERSION = "0.1dev";
+	public static final String VERSION = "0.2dev";
 
 	public static final String BLOCKTEX_PNG = "elizacat/mc/StainedGlass/block.png";
 
