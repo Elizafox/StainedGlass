ae390770f438512ae1ab53a759e673ff5561b33e
Elizabeth J. Myers <elizabeth@sporksmoo.net>
2012-12-29 16:48:56 -0600

    0.3.1

--- common/Common.java
+++ common/Common.java
@@ -18,7 +18,7 @@ import elizacat.mc.StainedGlass.proxy.BlockStainedPane;
 
 public class Common {
 	public static final String NAME = "StainedGlass";
-	public static final String VERSION = "0.3dev";
+	public static final String VERSION = "0.3.1dev";
 
 	public static final String BLOCKTEX_PNG = "elizacat/mc/StainedGlass/block.png";
 
